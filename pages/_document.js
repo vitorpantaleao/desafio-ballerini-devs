6659d6dacb3d629441f570c8eb2c49eb918ed0ce
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-18 00:38:50 -0300

    section home

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default function DocumentApp() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
